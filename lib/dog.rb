73ada7337575047f7c71b39c0e0257f47c800589
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-16 02:12:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog 
+@@all =[]
+attr_accessor :name 
+def initialize(name)
+@name = name 
+save(name)
+end 
+def self.all 
+  @@all 
+end
+def self.print_all 
+@@all.each do |value|
+  puts value 
+end 
+end 
+def save(name)
+@@all << name 
+end 
+def self.clear_all 
+@@all.clear 
+end 
+end 
