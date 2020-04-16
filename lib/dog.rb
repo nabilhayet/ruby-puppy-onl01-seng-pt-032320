c1398c5e854f677a9d0c1366731ba3a3f16d1023
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-16 02:46:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 require "pry"
 class Dog 
-@@all =[]
-attr_accessor :name 
-def initialize(name)
-@name = name 
-self.save
-end 
+  @@all =[]
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    self.save
+  end 
+  
 def self.all 
   @@all 
 end
